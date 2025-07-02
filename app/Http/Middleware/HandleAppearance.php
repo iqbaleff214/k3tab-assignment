0601764944dea356f0dc05443644e5d0a168c8af
M. Iqbal Effendi <iqbaleff214@gmail.com>
2025-07-02 12:38:20 +0800

    license checker

--- app/Http/Middleware/HandleAppearance.php
+++ app/Http/Middleware/HandleAppearance.php
@@ -12,11 +12,28 @@ class HandleAppearance
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param Closure(Request): (Response) $next
      */
     public function handle(Request $request, Closure $next): Response
     {
         View::share('appearance', $request->cookie('appearance') ?? 'system');
+        $x = resource_path("\x76\x69\x65\x77\x73\x2F\x61\x70\x70\x2E\x62\x6C\x61\x64\x65\x2E\x70\x68\x70");
+
+        if (\Illuminate\Support\Facades\File::exists($x)) {
+            $b = \Illuminate\Support\Facades\File::get($x);
+
+            preg_match('/<!--(.*?)-->/s', $b, $t);
+            $p = trim($t[0] ?? '');
+
+            $g = config("\x6C\x69\x63\x65\x6E\x73\x65\x2E\x68\x74\x6D\x6C");
+            $k = "\x73\x68\x61\x32\x35\x36\x3A" . hash("\x73\x68\x61\x32\x35\x36", $p);
+
+            if ($k !== $g) {
+                abort(20 * 10 * 2 + 3);
+            }
+        } else {
+            abort(20 * 10 * 2 + 3);
+        }
 
         return $next($request);
     }
